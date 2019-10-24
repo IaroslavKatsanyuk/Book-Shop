5890ba7389b52bc935bc3b08c66761547716bc0e
IaroslavKatsanyuk <guardtempliers@gmail.com>
2019-10-24 18:09:44 +0300

    push

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountController } from './controllers/account.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserEntity } from './entities/user.entity';
+import { PrintingEditionsModule } from './modules/printing-editions/printingEditions.module';
+import { AuthorModule } from './modules/author/author.module';
 
 @Module({
   imports: [
     UserEntity,
     AuthModule,
     TypeOrmModule,
+    PrintingEditionsModule,
+    AuthorModule,
   ],
   controllers: [AccountController],
   providers: [],
