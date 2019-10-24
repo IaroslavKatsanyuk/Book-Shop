a9b0fd5df68b9c0f64de0e556e59ab6a7381d1c3
IaroslavKatsanyuk <guardtempliers@gmail.com>
2019-10-24 18:06:38 +0300

    autor commit

--- server/src/repositories/connectionDatabase.ts
+++ server/src/repositories/connectionDatabase.ts
@@ -1,5 +1,8 @@
 import { UserEntity } from "../entities/user.entity";
 import { createConnection } from "typeorm";
+import { AuthorEntity } from "src/entities/author.entity";
+import { AuthorInBooksEntity } from "src/entities/authorInBooks.entity";
+import { PrintingEditionsEntity } from "src/entities/printingEditions.entity";
 
 export const databaseProviders = [
     {
@@ -11,7 +14,7 @@ export const databaseProviders = [
             username: 'BookShopUser',
             password: 'Ukraine1990',
             database: 'my-db',
-            entities: [UserEntity],
+            entities: [UserEntity, PrintingEditionsEntity, AuthorEntity, AuthorInBooksEntity],
             synchronize: true,
         }),
     }]
