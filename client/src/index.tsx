e00b8f7679179f50470622fd44e5a6f1686c3a9d
IaroslavKatsanyuk <guardtempliers@gmail.com>
2019-11-07 14:18:50 +0200

    Client add

---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'
+
+
+function BookShop (){
+    return (
+        <span>Hello World</span>
+    );
+}
+
+
+ReactDOM.render(<BookShop />, document.getElementById('root'))
